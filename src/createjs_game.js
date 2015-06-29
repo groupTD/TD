43f84f46b8d6abebd4a9d73acf6e73d91fb08439
big_dido <big_dido@yahoo.com>
2015-06-30 00:20:10 +0300

    Fixed firefox mouseHandler

--- src/createjs_game.js
+++ src/createjs_game.js
@@ -68,9 +68,8 @@ function initGame() {
 }
 
 function mouseHandler(event) {
-	//console.log(event.x + " " + event.y);
     if (!game.paused) {
-        var tile = Entity.prototype.getTile(game.grid, event.x, event.y);
+        var tile = Entity.prototype.getTile(game.grid, event.pageX, event.pageY);
         if (undefined != tile) {
             game.addTower(tile.x, tile.y);
             stage.update();
